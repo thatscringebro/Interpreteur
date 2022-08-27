7e8b12bdd28a33dfdf85ee862e2b6729b860a5f5
thatscringebro <thatscringebro@tutanota.com>
2022-08-26 21:43:25 -0400

    Ajout des logiques principales

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <stack>
+
+std::stack<char> st;
 
 int main(){
     
@@ -6,39 +9,50 @@ int main(){
 }
 
 void P(int val){
-
+    st.push(val);
 }
 
 void p(){
-
+    st.pop();
 }
 
 void d(){
-
+    st.push(st.top());
 }
 
 void plus(){
-
+    int val1 = st.top();
+    st.pop();
+    int val2 = st.top();
+    st.pop();
+    st.push(val1 + val2);
 }
 
 void minus(){
-
+    int val1 = st.top();
+    st.pop();
+    int val2 = st.top();
+    st.pop();
+    st.push(val2 - val1);
 }
 
 void r(char val[]){
 
 }
 
-void R(int val){
-
+void R(){
+    int val;
+    std::cin >> val;
+    st.push(val);
 }
 
 void w(){
-
+    
 }
 
 void W(){
-
+    std::cout << st.top();
+    st.pop();
 }
 
 void Z(){
