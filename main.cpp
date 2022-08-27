7a0ab52bb5d694d647106863581fe6198d66561c
thatscringebro <thatscringebro@tutanota.com>
2022-08-26 21:27:38 -0400

    Layout des fonctions

--- main.cpp
+++ main.cpp
@@ -4,3 +4,43 @@ int main(){
     
     return 0;
 }
+
+void P(int val){
+
+}
+
+void p(){
+
+}
+
+void d(){
+
+}
+
+void plus(){
+
+}
+
+void minus(){
+
+}
+
+void r(char val[]){
+
+}
+
+void R(int val){
+
+}
+
+void w(){
+
+}
+
+void W(){
+
+}
+
+void Z(){
+
+}
